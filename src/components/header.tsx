4a6f65962c6e7e7fe82baa5e15085729fef7b2c2
miraclemenikelechi <miracle.emenike7@gmail.com>
2024-11-20 09:59:32 +0100

    Squashed 'frontend/' changes from bb56877..7b05baf
    7b05baf footer component
    9e419c1 header component
    
    git-subtree-dir: frontend
    git-subtree-split: 7b05baf9400730215b855fe11573dc081eea8364

---
+++ src/components/header.tsx
@@ -0,0 +1,69 @@
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { ArrowRight } from "lucide-react";
+
+import { Button } from "./ui/button";
+import {
+	NavigationMenu,
+	NavigationMenuItem,
+	NavigationMenuLink,
+	NavigationMenuList,
+	navigationMenuTriggerStyle,
+} from "./ui/navigation-menu";
+
+const navigationLinks: {
+	title: string;
+	href: string;
+}[] = [
+	{ title: "Home", href: "/" },
+	{ title: "Projects", href: "/" },
+	{ title: "Dashboard", href: "/" },
+	{ title: "Portfolio", href: "/" },
+	{ title: "Staking", href: "/" },
+	{ title: "Missions", href: "/" },
+];
+
+export default function Component() {
+	return (
+		<header className={`w-full flex items-center justify-center px-[2rem] py-4`}>
+			<div className={`h-[2.5rem] flex items-center justify-between max-w-[91.75rem] w-full`}>
+				<Link href="/">logo</Link>
+
+				<NavigationMenu>
+					<NavigationMenuList className={`gap-3`}>
+						{navigationLinks.map(function (value, index) {
+							return (
+								<NavItem
+									key={index}
+									{...value}
+								/>
+							);
+						})}
+					</NavigationMenuList>
+				</NavigationMenu>
+
+				<Button className={`rounded-full`}>
+					<span>Login</span>
+					<ArrowRight />
+				</Button>
+			</div>
+		</header>
+	);
+}
+
+function NavItem({ title, href }: { title: string; href: string }) {
+	return (
+		<NavigationMenuItem className={`rounded-full overflow-hidden`}>
+			<Link
+				href={href}
+				legacyBehavior
+				passHref
+				className={`bg-transparent`}
+			>
+				<NavigationMenuLink className={navigationMenuTriggerStyle()}>
+					{title}
+				</NavigationMenuLink>
+			</Link>
+		</NavigationMenuItem>
+	);
+}
--- src/contexts/wrapper.tsx
+++ src/contexts/wrapper.tsx
@@ -1,3 +1,5 @@
+import { Toaster } from "sonner";
+import { ThemeProvider } from "next-themes";
 import React, { Fragment, ReactNode } from "react";
 
 import AppWalletProvider from "./AppWalletProvider";
@@ -5,7 +7,15 @@ import AppWalletProvider from "./AppWalletProvider";
 export default function Component({ children }: { children: ReactNode }) {
 	return (
 		<Fragment>
-			<AppWalletProvider>{children}</AppWalletProvider>
+			<AppWalletProvider>
+				<ThemeProvider
+					enableSystem
+					attribute="class"
+				>
+					{children}
+				</ThemeProvider>
+			</AppWalletProvider>
+			<Toaster />
 		</Fragment>
 	);
 }
