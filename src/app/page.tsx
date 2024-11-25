c77d8891a7c61cf534ca430ea236514aca45eb9a
miraclemenikelechi <miracle.emenike7@gmail.com>
2024-11-25 21:34:56 +0100

    Squashed 'frontend/' changes from 988ca27..3b59641
    3b59641 projects page init
    ce2b0ab adjust layout to fit main content
    
    git-subtree-dir: frontend
    git-subtree-split: 3b596418e64f6e6ca147479a83bcf62ccbb6a6ee

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Home() {
+	return (
+		<main className="flex items-center justify-center">
+			<h1 className="text-3xl underline">hello world</h1>
+		</main>
+	);
+}
---
+++ src/app/projects/page.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Page() {
+	return (
+		<section className={``}>
+			<div>
+				<h2>Upcoming Projects</h2>
+
+				<aside></aside>
+			</div>
+
+			<div>
+				<section>
+					<h2>Funded Projects</h2>
+					<span>We bring new technologies to our community</span>
+				</section>
+
+				<aside></aside>
+			</div>
+		</section>
+	);
+}
